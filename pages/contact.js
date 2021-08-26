050cf7ef17bf192cb1d0d0cead2155be59f1dc5e
Rahul5430 <rahul213himani@gmail.com>
2021-08-26 11:35:15 +0530

    added pages

---
+++ pages/contact.js
@@ -0,0 +1,38 @@
+import { signIn, signOut, useSession } from 'next-auth/client';
+import NavBar from "../components/NavBar";
+import Footer from '../components/Footer';
+
+const Home = () => {
+    const [session, loading] = useSession();
+
+    const data = (user) => {
+        var name, email, sid, branch, img;
+    
+        name = user.name;
+        email = user.email;
+        img = user.image;
+    
+        var index1 = user.email.indexOf('.');
+        var index2 = user.email.indexOf('@');
+        branch = user.email.slice(index1+3, index2-2).toUpperCase();
+        sid = (user.name).startsWith('bt2') ? (user.name).slice(2, 10) : '';
+    
+        return {
+            name: name,
+            email: email,
+            sid: sid,
+            branch: branch,
+            image: img,
+        };
+    };
+
+    return (
+        <>
+            <NavBar user={data(session.user)} />
+            <div>Contact</div>
+            <Footer />
+        </>
+    );
+};
+
+export default Home;
